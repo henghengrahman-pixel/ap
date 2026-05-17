d0c46a9153cf6a358a7bff6bdbe0c44c96da56c9
PINK <henghengrahman@gmail.com>
2026-05-18 03:47:45 +0700

    Delete services directory

--- services/fcm.js
+++
@@ -1,8 +0,0 @@
-async function sendBroadcast(title, body) {
-  console.log('Push notification disabled. Internal inbox is active:', title, body);
-  return true;
-}
-function initFirebase() {
-  console.log('Firebase disabled. Using internal inbox messages.');
-}
-module.exports = { initFirebase, sendBroadcast };
