f04f60ecbbe8bdad1fa9e52b65f0f4a7ee08f903
PINK <henghengrahman@gmail.com>
2026-05-18 22:25:18 +0700

    Delete public directory

--- public/firebase-messaging-sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js');
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  const url = event.notification.data && event.notification.data.url ? event.notification.data.url : '/';
-  event.waitUntil(clients.openWindow(url));
-});
